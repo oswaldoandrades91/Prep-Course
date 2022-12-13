42c9c102175ba68ff71cd67b179ddd6fb3d1f7fc
oswaldoandrades91 <119514542+oswaldoandrades91@users.noreply.github.com>
2022-12-12 20:50:42 -0600

    pinche vida  de pobre

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -88,12 +88,22 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero===10  ||  numero ===5 ) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero>20  &&  numero  <50 ) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function esEntero(numero) {
